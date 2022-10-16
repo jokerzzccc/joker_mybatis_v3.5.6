05869e4ea9b4449c7d7145b483243ce6af055cf3
joker <joker_monster@foxmail.com>
2022-10-16 23:18:21 +0800

    feat: finish-joker-mybatis-01 创建简单的映射器代理工厂

---
+++ joker-mybatis-01/src/test/java/com/joker/mybatis/test/dao/IUserDao.java
@@ -0,0 +1,17 @@
+package com.joker.mybatis.test.dao;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author jokerzzccc
+ * @date 2022/10/16
+ */
+public interface IUserDao {
+
+    String queryUserName(String uId);
+
+    String queryUserAge(String uId);
+
+}
